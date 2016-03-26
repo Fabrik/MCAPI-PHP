f8bb149da6876517a31f490df775c0b145a52c51
Seshachalam Malisetti <seshachalam@outlook.com>
2016-03-26 17:12:16 +0530

    Update MAPI.class.php
    Some people are still using it.

--- MAPI.class.php
+++ MAPI.class.php
@@ -36,7 +36,7 @@ class MCAPI {
      * @param string $apikey Your MailChimp apikey
      * @param string $secure Whether or not this should use a secure connection
      */
-    function MCAPI($apikey, $secure=false) {
+    public function __construct($apikey, $secure=false) {
         $this->secure = $secure;
         $aAPIKey = explode("-", $apikey);
         $apikey = sprintf("http://%s.api.mailchimp.com/%.1f/?output=php", $aAPIKey[1], $this->version);
